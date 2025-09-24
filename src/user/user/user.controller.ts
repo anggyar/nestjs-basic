1d88b72146c5b1919a953d03b62cf74538b88ca3
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2025-09-25 01:06:05 +0700

    Implement Cookie on app, now user can set and get cookies from the app

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -8,12 +8,25 @@ import {
   Post,
   Query,
   Redirect,
+  Req,
+  Res,
 } from '@nestjs/common';
-import { Response } from 'express';
+import { Request, Response } from 'express';
 // import express from 'express';
 
 @Controller('/api/users')
 export class UserController {
+  @Get('/set-cookie')
+  setCookie(@Query('name') name: string, @Res() response: Response) {
+    response.cookie('name', name);
+    response.status(200).send('Success Set Cookie');
+  }
+
+  @Get('/get-cookie')
+  getCookie(@Req() request: Request): string {
+    return request.cookies['name'] as string;
+  }
+
   @Get('/sample-response')
   @Header('Content-Type', 'application/json')
   @HttpCode(200)
